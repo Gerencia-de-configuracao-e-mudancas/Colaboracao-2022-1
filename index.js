650a3c0ca1e50d40dd9f554a38971fc3a14c74ba
mikael <mikael.stl05@gmail.com>
2022-06-10 22:01:08 -0300

    adicionando classe e criando objetos para armazenar dados de cada comando

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const Searchbar = document.querySelector("#searchBar");
+const nomes = document.querySelectorAll('h4.nome');
+const descricoes = document.querySelectorAll('p.descrição');
+const comandos = [];
+
+class Comando{
+    constructor(nome, descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+}
+
+function pegarNomes() {
+    for (const n of nomes) {
+        const nome = n.textContent;
+        const comando = new Comando(nome);
+        comandos.push(comando);
+    };    
+}
 
 Searchbar.addEventListener('focus', () => {
     Searchbar.style.boxShadow = "2px 2px 20px rgba(17, 2, 26, 0.5)";
